cf97834a9310820f7d6457db833158ad1837ccba
Esteban <luisesteban0406@gmail.com>
2025-03-21 21:27:10 -0500

    Part 3 - b complete

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.static('dist'))
