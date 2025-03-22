8f0d243d39abe9f9afd14bd2d88ed35c565b179d
Esteban <luisesteban0406@gmail.com>
2025-03-21 19:16:46 -0500

    Ready to deply in render

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
-
+const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
 
 let notes = [
   {
@@ -20,7 +25,7 @@ let notes = [
   },
 ];
 
-app.use(express.json())
+
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -37,56 +42,51 @@ app.get("/api/notes/:id", (request, response) => {
   note ? response.json(note) : response.status(404).end();
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+app.put("/api/notes/:id", (request, response) => {
+  const body = request.body;
+  const id = request.params.id;
+  notes = notes.map((note) => (note.id !== id ? note : body));
+  response.json(body);
+});
+
+app.post("/api/notes/", (request, response) => {
+  const body = request.body;
 
   if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
+    return response.status(400).json({
+      error: "content missing",
+    });
   }
 
   const note = {
     content: body.content,
     important: body.important || false,
     id: generateId(),
-  }
+  };
 
-  notes = notes.concat(note)
+  notes = notes.concat(note);
 
-  response.json(note)
-})
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  response.json(note);
+});
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-
-app.use(requestLogger)
-app.use(unknownEndpoint)
-
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
