1ad70376934fc48162823a98f63e516e7209d97e
Esteban <luisesteban0406@gmail.com>
2025-03-24 00:59:42 -0500

    Exercise complete

--- backend/index.js
+++ backend/index.js
@@ -6,30 +6,6 @@ const Person = require("./models/person");
 const app = express();
 app.use(express.static("dist"));
 app.use(morgan("dev"));
-
-let people = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.use(express.json());
 
 app.get("/", (request, response) => {
@@ -50,28 +26,28 @@ app.get("/api/people/:id", (request, response) => {
 });
 
 app.delete("/api/people/:id", (request, response) => {
-  
-  Person.findById(request.params.id).then((person) => {
-    person.delete().then((result) => {
-      response.status(204).end();
-    });
-  });
+  Person.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 });
 
 app.post("/api/people", (request, response) => {
   const body = request.body;
-  const person = new Person({ 
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: "name or number missing" });
+  }
+
+  const person = new Person({
     name: body.name,
-    number:body.number,
+    number: body.number,
   });
 
-  !body.name || !body.number
-    ? response.status(400).json({ error: "content missing" })
-    : people.find((person) => person.name === body.name)
-    ? response.status(400).json({ error: "name must be unique" })
-    : person.save().then((savedPerson) => {
-      response.json(savedPerson);
-    });
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
 app.put("/api/people/:id", (request, response) => {
@@ -83,8 +59,11 @@ app.put("/api/people/:id", (request, response) => {
     id: id,
   };
 
-  people = people.map((person) => (person.id !== id ? person : person));
-  response.json(person);
+  Person.findByIdAndUpdate
+    ? Person.findByIdAndUpdate(id, person, { new: true }).then((updatedPerson) => {
+        response.json(updatedPerson);
+      })
+    : response.status(404).end();
 });
 
 app.get("/info", (request, response) => {
@@ -97,3 +76,16 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
