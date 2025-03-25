8ebcd61c1f417eb7a453ce606a098083c0f970bc
Esteban <luisesteban0406@gmail.com>
2025-03-25 13:42:01 -0500

    Excericese complete

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-const express = require("express");
-const Person = require("./models/person");
+require('dotenv').config()
+const express = require('express')
+const Person = require('./models/person')
 
-const app = express();
+const app = express()
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -24,74 +24,68 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-app.use(express.static("dist"));
-app.use(express.json());
+app.use(express.static('dist'))
+app.use(express.json())
 app.use(requestLogger)
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
 
-app.get("/api/people", (request, response) => {
+app.get('/api/people', (request, response) => {
   Person.find({}).then((people) => {
-    response.json(people);
-  });
-});
-
-app.get("/api/people/:id", (request, response) => {
-  const id = request.params.id;
-  Person.findById(id).then((person) => {
-    person ? response.json(person) : response.status(404).end();
-  });
-});
-
-app.delete("/api/people/:id", (request, response) => {
-  Person.findByIdAndDelete(request.params.id)
-  .then(result => {
-    response.status(204).end()
+    response.json(people)
   })
-  .catch(error => next(error))
-});
+})
 
-app.post("/api/people", (request, response, next) => {
-  const body = request.body;
+app.get('/api/people/:id', (request, response) => {
+  const id = request.params.id
+  Person.findById(id).then((person) => {
+    person ? response.json(person) : response.status(404).end()
+  })
+})
+
+app.delete('/api/people/:id', (request, response,next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/people', (request, response, next) => {
+  const body = request.body
 
   if (!body.name || !body.number) {
-    return response.status(400).json({ error: "name or number missing" });
+    return response.status(400).json({ error: 'name or number missing' })
   }
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   person.save().then((savedPerson) => {
     response.json(savedPerson)
   })
-  .catch(error => next(error))
-});
+    .catch(error => next(error))
+})
 
-app.put("/api/people/:id", (request, response) => {
-  const id = request.params.id;
-  const body = request.body;
+app.put('/api/people/:id', (request, response) => {
+  const id = request.params.id
+  const body = request.body
   const person = {
     name: body.name,
     number: body.number,
     id: id,
-  };
+  }
 
   Person.findByIdAndUpdate
     ? Person.findByIdAndUpdate(id, person, { new: true }).then((updatedPerson) => {
-        response.json(updatedPerson);
-      })
-    : response.status(404).end();
-});
-
-app.get("/info", (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${people.length} people</p><p>${new Date()}</p>`
-  );
-});
+      response.json(updatedPerson)
+    })
+    : response.status(404).end()
+})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -100,7 +94,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- backend/models/person.js
+++ backend/models/person.js
@@ -7,7 +7,7 @@ const url = process.env.API_KEY
 
 mongoose.connect(url)
 
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch(error => {
@@ -27,7 +27,7 @@ const personSchema = new mongoose.Schema({
     minlength: 8,
     validate:{
       validator: function(v){
-        return /^\d{2,3}-\d+$/.test(v);
+        return /^\d{2,3}-\d+$/.test(v)
       },
       message: props => `${props.value} is not a valid phone number!`
     }
