6784a87cbb9f4e0480aa6fd37c65f032c9b3d5df
Esteban <luisesteban0406@gmail.com>
2025-03-25 13:25:38 -0500

    Add Eslint

--- backend/index.js
+++ backend/index.js
@@ -87,11 +87,12 @@ app.put('/api/notes/:id', (request, response, next) => {
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-    .then((result) => {
+    .then(() => {
       response.status(204).end()
     })
     .catch((error) => next(error))
 })
+
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
