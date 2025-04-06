6545def1d11befac3e1ccac6c0a8ca9b8b7a64d8
Esteban <luisesteban0406gmail.com>
2025-04-06 00:03:43 -0500

    excercise complete

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -12,4 +12,20 @@ blogRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog)
 })
 
+blogRouter.delete('/:id', async (request, response) => {
+  const blog = new Blog(request.body)
+  const savedBlog = await blog.deleteOne()
+  response.json(savedBlog)
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const { id } = request.params
+  const updatedData = request.body
+
+  const updatedBlog = await Blog.findByIdAndUpdate(id, updatedData, { new: true, runValidators: true })
+  updatedBlog?   response.json(updatedBlog)   :    response.status(404).json({ error: 'Blog not found' })
+
+})
+
+
 module.exports = blogRouter
--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -32,12 +32,12 @@ describe('Blog API tests', () => {
     }
 
     await api
-      .post('/api/Blogs')
+      .post('/api/blogs')
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
-    const response = await api.get('/api/Blogs')
+    const response = await api.get('/api/blogs')
 
     const blogs = response.body
 
@@ -50,7 +50,7 @@ describe('Blog API tests', () => {
   })
 
   test('Id IS named ID', async () => {
-    const response = await api.get('/api/Blogs')
+    const response = await api.get('/api/blogs')
 
     assert.ok(response.body[0].id, 'there is nnot __ID')
   })
@@ -63,12 +63,12 @@ describe('Blog API tests', () => {
     }
 
     await api
-      .post('/api/Blogs')
+      .post('/api/blogs')
       .send(newBlog)
       .expect(201)
       .expect('Content-Type', /application\/json/)
 
-    const response = await api.get('/api/Blogs')
+    const response = await api.get('/api/blogs')
 
     const addedBlog = response.body.find(
       (b) => b.title === newBlog.title && b.author === newBlog.author
@@ -94,15 +94,75 @@ describe('Blog API tests', () => {
     }
 
     await api
-      .post('/api/Blogs')
+      .post('/api/blogs')
       .send(newBlogWithoutUrl)
       .expect(400)
       .expect('Content-Type', /application\/json/)
 
     await api
-      .post('/api/Blogs')
+      .post('/api/blogs')
       .send(newBlogWithoutTitle)
       .expect(400)
       .expect('Content-Type', /application\/json/)
   })
+
+  test('can delete a blog', async () => {
+    const newBlogWithoutUrl = {
+      title: 'El juan agogo3',
+      author: 'juan agogo3',
+      url: 'http://www.juanagogo3.com',
+      id:'123456'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlogWithoutUrl)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    await api
+      .delete('/api/blogs/123456')
+      .send(newBlogWithoutUrl)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('can update a blog', async () => {
+    // Create a new blog
+    const newBlog = {
+      title: 'El juan agogo4',
+      author: 'juan agogo4',
+      url: 'http://www.juanagogo4.com',
+    }
+
+    // Add the blog to the database
+    const response = await api
+      .post('/api/blogs') // Use lowercase 'blogs' to match the route
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    // Extract the ID of the newly created blog
+    const blogToUpdate = response.body
+
+    // Prepare updated data
+    const updatedBlog = {
+      title: 'Updated Title',
+      author: 'Updated Author',
+      url: 'http://www.updatedurl.com',
+    }
+
+    // Update the blog
+    const updateResponse = await api
+      .put(`/api/blogs/${blogToUpdate.id}`) // Use the correct ID
+      .send(updatedBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    // Verify the update
+    assert.strictEqual(updateResponse.body.title, updatedBlog.title)
+    assert.strictEqual(updateResponse.body.author, updatedBlog.author)
+    assert.strictEqual(updateResponse.body.url, updatedBlog.url)
+  })
+
 })
