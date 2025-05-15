b71582aeb66f6aaab7a8716368b5db11740687ef
Esteban <luisesteban0406@gmail.com>
2025-05-15 08:29:13 -0500

    Part a complete

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,55 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import  Notes  from "./Notes";
+import NewNote from "./NewNote";
 
-function App() {
-  const counterReducer = (state = 0, action) => {
-    switch (action.type) {
-      case 'INCREMENT':
-        return state + 1;
-      case 'DECREMENT':
-        return state - 1;
-      case 'ZERO':
-        return 0;
-      default:
-        return state;
-    }
-  };
-
-  const store = createStore(counterReducer);
-  const [count, setCount] = useState(store.getState()); // Estado local para el contador
-
-  useEffect(() => {
-    // Función listener que se ejecuta cuando el estado de Redux cambia
-    const handleChange = () => {
-      setCount(store.getState()); // Actualiza el estado local con el nuevo valor del store
-    };
-
-    // Suscribe el listener al store
-    const unsubscribe = store.subscribe(handleChange);
-
-    // Función de limpieza que se ejecuta cuando el componente se desmonta
-    return () => {
-      unsubscribe(); // Desuscribe el listener para evitar fugas de memoria
-    };
-  }, [store]); // Dependencia en el store (aunque generalmente no cambia)
+const App = () => {
 
   return (
-    <>
-      <div>
-        {count} {/* Muestra el valor del estado local */}
-      </div>
-      <button onClick={() => store.dispatch({ type: 'INCREMENT' })}>
-        plus
-      </button>
-      <button onClick={() => store.dispatch({ type: 'DECREMENT' })}>
-        minus
-      </button>
-      <button onClick={() => store.dispatch({ type: 'ZERO' })}>
-        zero
-      </button>
-    </>
-  );
+    <div>
+      <NewNote />
+      <Notes />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import noteReducer from './reducers/noteReducer'
 
-
+const store = createStore(noteReducer)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+     <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>,
 )
