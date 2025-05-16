19e0cbddad2bb871da5217d863d2a68b72769afd
Esteban <luisesteban0406@gmail.com>
2025-05-16 17:45:19 -0500

    Part 6 B

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,13 @@
-import  Notes  from "./Notes";
-import NewNote from "./NewNote";
+import Notes from './components/Notes'
+import NewNote from './components/NewNote'
+import VisibilityFilter from './components/VisibilityFilter'
 
 const App = () => {
-
+  
   return (
     <div>
       <NewNote />
+       <VisibilityFilter />
       <Notes />
     </div>
   )
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import noteReducer from './reducers/noteReducer'
+import filterReducer from './reducers/filterReducer'
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+})
+
+const store = createStore(reducer)
+console.log(store.getState())
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
