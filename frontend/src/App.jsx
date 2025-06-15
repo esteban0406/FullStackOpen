39dfc50dfe1e9ce7e2276ff53aa24f6801996d93
Esteban <luisesteban0406@gmail.com>
2025-06-14 23:42:08 -0500

    Part d

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,23 @@ import PersonForm from './components/PersonForm.jsx'
 import { ALL_PERSONS } from './queries'
 import { useState } from 'react'
 import PhoneForm from './components/PhoneForm.jsx'
+import LoginForm from './components/LoginForm.jsx'
+import { useApolloClient } from '@apollo/client'
 
 const App = () => {
+  const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
-
   const result = useQuery(ALL_PERSONS)
+  const client = useApolloClient()
 
   if (result.loading) {
     return <div>loading...</div>
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
   }
 
   const notify = (message) => {
@@ -21,9 +30,19 @@ const App = () => {
     }, 10000)
   }
 
+  if (!token) {
+    return (
+      <>
+        <Notify errorMessage={errorMessage} />
+        <LoginForm setToken={setToken} setError={notify} />
+      </>
+    )
+  }
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,35 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import {
-  ApolloClient,
-  InMemoryCache,
-  gql,
-  ApolloProvider,
-} from '@apollo/client'
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
-})
-
-const query = gql`
-  query {
-    allPersons {
-      name
-      phone
-      address {
-        street
-        city
-      }
-      id
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('phonenumbers-user-token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
     }
   }
-`
-
-client.query({ query }).then((response) => {
-  console.log(response.data)
 })
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+})
+
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink)
+})
+
 
 createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
