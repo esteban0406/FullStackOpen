6af576cd92ddf9d63e15428f53c4bd45f3bc3f4d
Esteban <luisesteban0406@gmail.com>
2025-06-20 10:07:46 -0500

    Part C

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,7 @@
 import express from 'express';
+
+import diaryRouter from './routes/diaries';
+
 const app = express();
 app.use(express.json());
 
@@ -9,6 +12,10 @@ app.get('/ping', (_req, res) => {
   res.send('pong');
 });
 
+
+app.use('/api/diaries', diaryRouter);
+
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
