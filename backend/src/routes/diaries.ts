2547e37c1933bdf44544dc889dd632cc76962aa2
Esteban <luisesteban0406@gmail.com>
2025-06-21 20:09:25 -0500

    Partt C before Exercises 9.12-9.13

--- backend/src/routes/diaries.ts
+++ backend/src/routes/diaries.ts
@@ -1,16 +1,50 @@
-import express from 'express';
-import diaryService from '../services/diaryService';
+import express from 'express'
+import diaryService from '../services/diaryService'
 import { Response } from 'express'
-import { NonSensitiveDiaryEntry } from "../types";
+import { NonSensitiveDiaryEntry } from '../types'
+import {toNewDiaryEntry} from '../utils';
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/', (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
-  res.send(diaryService.getNonSensitiveEntries());
-});
+  res.send(diaryService.getNonSensitiveEntries())
+})
 
-router.post('/', (_req, res) => {
-  res.send('Saving a diary!');
-});
+router.post('/', (req, res) => {
+  const { date, weather, visibility, comment } = req.body
 
-export default router;
+  const addedEntry = diaryService.addDiary({
+    date,
+    weather,
+    visibility,
+    comment,
+  })
+  res.json(addedEntry)
+})
+
+router.get('/:id', (req, res) => {
+  const diary = diaryService.findById(Number(req.params.id))
+
+  if (diary) {
+    res.send(diary)
+  } else {
+    res.sendStatus(404)
+  }
+})
+
+router.post('/', (req, res) => {
+  try {
+    const newDiaryEntry = toNewDiaryEntry(req.body);
+
+    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
+})
+
+export default router
--- backend/src/services/diaryService.ts
+++ backend/src/services/diaryService.ts
@@ -1,10 +1,9 @@
 import diaries from '../../data/entries'
+import { NewDiaryEntry, NonSensitiveDiaryEntry, DiaryEntry } from '../types';
 
-import { NonSensitiveDiaryEntry, DiaryEntry } from '../types'
-
-const getEntries = () : DiaryEntry[] => {
+const getEntries = (): DiaryEntry[] => {
   return diaries
-};
+}
 
 const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   return diaries.map(({ id, date, weather, visibility }) => ({
@@ -15,12 +14,24 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
   }))
 }
 
-const addDiary = () => {
-  return null;
+const findById = (id: number): DiaryEntry | undefined => {
+  const entry = diaries.find((d) => d.id === id)
+  return entry
+}
+
+const addDiary = ( entry: NewDiaryEntry ): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map(d => d.id)) + 1,
+    ...entry
+  };
+
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
 };
 
 export default {
   getEntries,
   getNonSensitiveEntries,
-  addDiary
+  addDiary,
+  findById,
 }
